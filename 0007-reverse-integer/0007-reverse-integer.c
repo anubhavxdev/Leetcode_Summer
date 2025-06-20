acde47c903feaafa68dd09e85bfc7a012b8e32db
Anubhav Jaiswal <149222708+anubhavxdev@users.noreply.github.com>
2025-06-20 20:20:14 +0530

    Time: 3 ms (55.45%), Space: 8.5 MB (72.1%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.c
@@ -0,0 +1,21 @@
+#include <limits.h> // for INT_MAX and INT_MIN
+
+int reverse(int x) {
+    int reversed = 0;
+    
+    while (x != 0) {
+        int digit = x % 10;
+
+        if (reversed > INT_MAX / 10 || (reversed == INT_MAX / 10 && digit > 7)) {
+            return 0;
+        }
+        if (reversed < INT_MIN / 10 || (reversed == INT_MIN / 10 && digit < -8)) {
+            return 0; 
+        }
+
+        reversed = reversed * 10 + digit;
+        x /= 10;
+    }
+    
+    return reversed;
+}
