94be5d609f294e4258fce207216bf152783f3b4b
Anubhav Jaiswal <149222708+anubhavxdev@users.noreply.github.com>
2025-06-25 22:55:31 +0530

    Time: 0 ms (100%), Space: 7.8 MB (16.92%) - LeetHub

---
+++ 0397-integer-replacement/0397-integer-replacement.c
@@ -0,0 +1,16 @@
+int integerReplacement(int n) {
+    long num = n; 
+    int count = 0;
+    while (num != 1) {
+        if ((num & 1) == 0) {
+            num >>= 1;
+        } else {
+            if (num == 3 || ((num >> 1) & 1) == 0)
+                num--;  
+            else
+                num++;
+        }
+        count++;
+    }
+    return count;
+}
